6b2815ac5d4bb4d5773b13c4526dbc34e8b19818
KoekEnPeer <rick@koekenpeer.nl>
2022-07-12 09:35:50 +0200

    Support for PostNL plugin.

--- src/Helper/MyParcelHelper.php
+++ src/Helper/MyParcelHelper.php
@@ -7,13 +7,15 @@ class MyParcelHelper
     CONST SHIPMENT_EXPORT_OPTION_TNT = 'tnt';
 
     /**
-     * Checks if the MyParcel plugin is activated (check both MyParcel NL and MyParcel BE plugins).
+     * Checks if the MyParcel plugin is activated (check MyParcel NL, MyParcel BE, PostNL plugins).
      * @return bool
      */
     public static function myParcelPluginActivated(): bool
     {
-        return function_exists('is_plugin_active')
-            && (is_plugin_active('woocommerce-myparcel/woocommerce-myparcel.php') || is_plugin_active('wc-myparcel-belgium/woocommerce-myparcel.php'))
+        return function_exists('is_plugin_active') && (
+                is_plugin_active('woocommerce-myparcel/woocommerce-myparcel.php')
+                || is_plugin_active('woo-postnl/woocommerce-postnl.php')
+            )
             ;
     }
 
@@ -27,6 +29,7 @@ class MyParcelHelper
         return
             (class_exists('WCMYPA_Admin') && $metaKey === \WCMYPA_Admin::META_SHIPMENTS) // MyParcel NL
             || (class_exists('WCMYPABE_Admin') && $metaKey === \WCMYPABE_Admin::META_SHIPMENTS) // MyParcel BE
+            || (class_exists('WCPOST_Admin') && $metaKey === \WCPOST_Admin::META_SHIPMENTS) // PostNL
             ;
     }
 }
