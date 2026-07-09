3bf8566e61568cf53b69c9abd276c08b769996a2
Rick Niesen <rick@koekenpeer.nl>
2026-07-09 14:54:29 +0200

    Support for EAN for WooCommerce 5.5.6.

--- src/Helper/EanForWooCommercePluginHelper.php
+++ src/Helper/EanForWooCommercePluginHelper.php
@@ -7,6 +7,11 @@ use WC_Product;
 
 /**
  * Helper functions for external plugin "EAN for WooCommerce" (https://wordpress.org/plugins/ean-for-woocommerce/).
+ *
+ * Note: as of plugin v5.5.6, the plugin's global accessor function was renamed
+ * from `alg_wc_ean()` to `wpfactory_wc_ean()` (prefixes changed from `alg` to `wpfactory`).
+ * The `->core->get_ean()` method itself kept the same signature, so we just need
+ * to call whichever accessor function currently exists.
  */
 class EanForWooCommercePluginHelper
 {
@@ -29,10 +34,34 @@ class EanForWooCommercePluginHelper
     public static function getValue(WC_Product $product): string
     {
         try {
-            $ean = strval(alg_wc_ean()->core->get_ean($product->get_id(), true));
+            $eanPlugin = self::getEanPluginInstance();
+            $ean       = $eanPlugin !== null
+                ? strval($eanPlugin->core->get_ean($product->get_id(), true))
+                : '';
         } catch (Throwable $e) {
             $ean = '';
         }
         return $ean;
+    }
+
+    /**
+     * Returns the main plugin instance, supporting both the new (>= 5.5.6)
+     * and old (< 5.5.6) accessor function names.
+     *
+     * @return object|null
+     */
+    private static function getEanPluginInstance()
+    {
+        if (function_exists('wpfactory_wc_ean')) {
+            // New version (>= 5.5.6): prefixes renamed from `alg` to `wpfactory`.
+            return wpfactory_wc_ean();
+        }
+
+        if (function_exists('alg_wc_ean')) {
+            // Old version (< 5.5.6).
+            return alg_wc_ean();
+        }
+
+        return null;
     }
 }
